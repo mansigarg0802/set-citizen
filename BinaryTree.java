8baaff6bd7966776e302b645f01b22a9619118c7
mansigarg0802 <mansirox0826@gmail.com>
2015-07-28 14:10:54 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -11,19 +11,8 @@ public class BinaryTree
    this.data=data;
   }
  }
- public static  int getLeafCountOfBinaryTree(TreeNode node)
-{
-  if(node == null)      
-    return 0;
-  if(node.n[0] ==null && node.n[1]==null&& node.n[2]==null&& node.n[3]==null&& node.n[4]==null)     
-    return 1;           
-  else
-    return getLeafCountOfBinaryTree(node.n[0])+ getLeafCountOfBinaryTree(node.n[1])+getLeafCountOfBinaryTree(node.n[2])+getLeafCountOfBinaryTree(node.n[3])+getLeafCountOfBinaryTree(node.n[4]);     
-}
-
 public static void main(String args[] ) throws Exception
 {
-
 Scanner sc = new Scanner(System.in);
 int test = sc.nextInt();int ppl;
 for(int i=0; i<test; i++)
@@ -38,17 +27,15 @@ arr[j]=sc.nextInt();
 arr[j+1]=sc.nextInt();
 }
 TreeNode rnode=createBinaryTree(arr);
-  System.out.println("Number of leaf nodes in binary tree :"+getLeafCountOfBinaryTree(rnode));
-
+printLevelOrder(rnode);
 }
 
 }
  public static TreeNode createBinaryTree(int[] arr)
  {
 int ppl=(arr.length/2)+1;
-System.out.println(ppl);
+
 TreeNode node[]=new TreeNode[ppl];
-System.out.println(node.length);
 for(int p=0;p<ppl;p++)
 {
 node[p]=new TreeNode(p);
@@ -68,4 +55,52 @@ x=0;
 }
   return node[0];
   }
+public static int heightOfBinaryTree(TreeNode node)
+{
+    if (node == null)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1 +
+        Math.max(heightOfBinaryTree(node.n[0]),
+            heightOfBinaryTree(node.n[1]));
+    }
 }
+public static void printLevelOrder(TreeNode root)
+{
+    int h = heightOfBinaryTree(root);    int i,odd=0,even=0,ans;
+ for (i=1; i<=h; i=i+2)
+    {
+       odd=odd+printGivenLevel(root, i);
+        
+    }
+    for (i=2; i<=h; i=i+2)
+    {
+        even=even+printGivenLevel(root, i);
+      
+    }
+ ans=Math.max(odd,even);
+System.out.println(ans);
+}
+ 
+public static int printGivenLevel(TreeNode root, int level)
+{int num1=0,num2=0;
+    if (root==null)
+        return 0;
+    if (level == 1)
+       return 1;
+    else if (level > 1)
+    {
+       num1=num1+printGivenLevel(root.n[0], level-1);
+        num2=num2+printGivenLevel(root.n[1], level-1);
+    }
+return num1+num2;
+}
+
+
+
+}
+
+
